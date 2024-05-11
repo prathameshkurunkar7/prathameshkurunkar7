0ff56c4c392857bd10294634a0ea9bd38e50f0d3
prathameshkurunkar7 <prathamesh.kurunkar@thecommit.company>
2024-05-11 14:08:42 +0530

    feat: added inline styling to render cards

--- render_blog_data.py
+++ render_blog_data.py
@@ -8,18 +8,31 @@ def render_blog_data():
         blog_data = json.load(f)
 
     # Function to generate the markdown for a single blog card
+    # Formatting of the function below is avoided to avoid extra spacing being generated in markdown
     def generate_blog_card(blog):
-        return f'<a href="{blog["url"]}" target="_blank"><img src="{blog["meta_image"]}" alt="{blog["title"]}" width="300"><h3>{blog["title"]}</h3><p>{blog["meta_description"]}</p></a>'
+        return f"""\n
+<a href="{blog["url"]}" target="_blank" style="text-decoration: none !important; color: inherit !important;">
+<div style="display: flex; align-items: center; border-radius: 4px; border: 1px solid #30363d; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); overflow: hidden; max-height: 200px;">
+<div style="max-width: 33.33%;">
+<img src="{blog["meta_image"]}" alt="{blog["title"]}" style="width: 100%; border-radius:4px">
+</div>
+<div style="padding: 16px;">
+<h2 style="font-size: 18px; font-weight: bold; margin-bottom: 10px;">{blog["title"]}</h2>
+<p style="font-size: 14px; line-height: 1.5; margin-bottom: 14px;">{blog["meta_description"]}</p>
+</div>
+</div>
+</a>\n"""
 
 
     # Generate the markdown for all blog cards
-    rendered_cards = ''.join([generate_blog_card(blog) for blog in [blog_data]])
+    rendered_cards = ''.join([generate_blog_card(blog) for blog in blog_data])
 
     # Wrap the rendered cards in a container
-    rendered_cards = f'<div align="center">{rendered_cards}</div>'
+    rendered_cards = f'<div style="display: flex; flex-direction: column; gap: 8px">{rendered_cards}</div>'
 
     # Append the rendered cards to the README file
     with open('README.md', 'a') as f:
+        f.write('\n\n')
         f.write(rendered_cards)
 
 if __name__ == '__main__':
